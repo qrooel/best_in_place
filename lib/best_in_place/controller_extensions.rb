6a8c2b5f040fc75b5e6378bafcc2364bb7a06d0f
Kris <qrooel@gmail.com>
2013-03-26 22:16:15 +0100

    Update controller_extensions.rb

--- lib/best_in_place/controller_extensions.rb
+++ lib/best_in_place/controller_extensions.rb
@@ -1,17 +1,19 @@
 module BestInPlace
   module ControllerExtensions
-    def respond_with_bip(obj)
-      obj.changed? ? respond_bip_error(obj) : respond_bip_ok(obj)
+    def respond_with_bip(obj, options={})
+      obj.changed? ? respond_bip_error(obj) : respond_bip_ok(obj, options)
     end
 
   private
-    def respond_bip_ok(obj)
-      if obj.respond_to?(:id)
+    def respond_bip_ok(obj, options)
+      if options[:class_name]
+        klass = options[:class_name]
+      elsif obj.respond_to?(:id)
         klass = "#{obj.class}_#{obj.id}"
       else
         klass = obj.class.to_s
       end
-      param_key = BestInPlace::Utils.object_to_key(obj)
+      param_key = options[:param_key] || BestInPlace::Utils.object_to_key(obj)
       updating_attr = params[param_key].keys.first
 
       if renderer = BestInPlace::DisplayMethods.lookup(klass, updating_attr)
